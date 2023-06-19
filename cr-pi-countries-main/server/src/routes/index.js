29094e0528f82c523ceda866182b069aebfd0bde
fedderica <fedderica.bartoli@gmail.com>
2023-06-19 14:45:38 -0300

    conexion-exitosa-modelos-rutas

---
+++ cr-pi-countries-main/server/src/routes/index.js
@@ -0,0 +1,12 @@
+const { Router } = require("express");
+
+const countries = require('./countries');
+const activities = require('./activities')
+const router = Router();
+
+router.use('/countries', countries);
+router.use('/activity', activities);
+
+
+
+module.exports = router;
